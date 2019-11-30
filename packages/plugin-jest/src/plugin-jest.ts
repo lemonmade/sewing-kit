8142ecdcde4e54f092495474642f59663ca25123
Chris Sauve <lemon@Chriss-MacBook-Pro.local>
2019-11-29 19:05:29 -0500

    Debug test plugin

--- packages/plugin-jest/src/plugin-jest.ts
+++ packages/plugin-jest/src/plugin-jest.ts
@@ -283,8 +283,10 @@ export default createPlugin(
           });
 
           const jest = await import('jest');
+          console.log(flags, toArgs(flags));
+          console.log(typeof jest, typeof jest?.default);
 
-          jest.default.run(toArgs(flags));
+          // jest.default.run(toArgs(flags));
         }),
       ]);
     });
